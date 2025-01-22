e196a37fa53ef3ddec98eec9d7a79609c75f0725
Dagmar87 <jdfssobrinho@hotmail.com>
2025-01-22 10:53:02 -0300

    Criação da Categoria View do arquivo views.py

--- teste_todolist/teste_todolist/views.py
+++ teste_todolist/teste_todolist/views.py
@@ -4,3 +4,7 @@ from .forms import FormTarefa, FormCategoria
 
 def index(request):
   return render(request, 'index.html')
+
+def categorias(request):
+  categorias = Categoria.objects.all()
+  return render(request, 'categorias.html', {'categorias': categorias})
