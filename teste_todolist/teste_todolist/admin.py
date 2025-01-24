77109fcbf71d5da73d2e2205e8dad085ba763585
Dagmar87 <jdfssobrinho@hotmail.com>
2025-01-24 16:43:24 -0300

    Revisão e ajustes no arquivo admin.py

--- teste_todolist/teste_todolist/admin.py
+++ teste_todolist/teste_todolist/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Categoria, Status, Tarefa
 
 class TarefaAdmin(admin.ModelAdmin):
-  list_display = ['categoria', 'status', 'descricao', 'data_de_vencimento']
+  list_display = ['categoria_id', 'status_id', 'titulo', 'descricao', 'data_de_vencimento']
   search_fields = ['titulo']
-  list_filter = ('categoria', 'status')
+  list_filter = ('categoria_id', 'status_id')
   list_per_page = 10
   
 admin.site.register(Tarefa, TarefaAdmin)
