ef48913ba2b287cfe3d01d68c0c061b4399c6056
Dagmar87 <jdfssobrinho@hotmail.com>
2025-01-24 17:56:53 -0300

    Revisão e adição das rotas no arquivo urls.py

--- teste_todolist/teste_todolist/urls.py
+++ teste_todolist/teste_todolist/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import index, TodoList_View, Categoria_View, tarefa_add, categoria_add, tarefa_edit, categoria_edit, status_change, tarefa_remove, categoria_remove
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('tarefa_list', TodoList_View),
+    path('categoria', Categoria_View),
+    path('tarefa/add', tarefa_add, name='tarefa_add'),
+    path('categoria/add', categoria_add, name='categoria_add'),
+
+    path('/<int:id>/remove', tarefa_remove, name='tarefa_remove'),
+    path('/<int:id>/remove_cat', categoria_remove, name='categoria_remove'),
+
+    path('/<int:id_tarefa>/<int:id_status>/status',
+         status_change, name='status_change'),
+    path('tarefa/<int:id>/edit', tarefa_edit, name='tarefa_edit'),
+    path('categoria/<int:id>/edit', categoria_edit, name='categoria_edit'),
 ]
